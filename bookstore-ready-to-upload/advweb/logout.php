9a77472497dc55177c2d7b41a931f52204287280
Luiza Miranda Gomes <15186@ait.nsw.edu.au>
2026-05-04 12:54:52 +1000

    Delete bookstore-ready-to-upload directory

--- bookstore-ready-to-upload/advweb/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Log out and return to the home page
-session_start();
-session_destroy();
-header("Location: index.php");
-exit();
-?>
--- bookstore-ready-to-upload/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Closes the session and redirects to the home page
-session_start();
-session_destroy();
-header("Location: index.php");
-exit();
-?>
